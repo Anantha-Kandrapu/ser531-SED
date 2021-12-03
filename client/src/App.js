c0e9e231bea4132dbc464ef3da4ee7a0cc572cc3
JayasaikalyanreddyTummuru <jtummuru@asu.edu>
2021-12-02 22:05:26 -0700

    Created Events page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './login/login';
 import Register from './login/register';
+import Event  from './events/events';
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
@@ -10,6 +11,8 @@ function App() {
           <Route exact path="/" element={<Login/>}/>
           <Route exact path="/sign-in" element={<Login/>}/>
           <Route exact path="/sign-up" element={<Register/>}/>
+          <Route exact path="/event" element={<Event/>}/>
+
         </Routes>
     </Router>
   );
---
+++ client/src/events/events.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+function Event(){
+
+return (
+    <div>
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>S.No</th>
+      <th>Event Name</th>
+      <th>Event Category</th>
+      <th>Event Location</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</Table>
+</div>
+)
+}
+export default Event
